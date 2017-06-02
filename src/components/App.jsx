db4e531ba39297da352fc7846f68ab20cf3f6035
solomontang <solomoncztang@gmail.com>
2017-06-02 15:55:33 -0700

    Refactor App into ES6 class that extends React.Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,28 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer videos={window.exampleVideoData[0]}/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // console.log(this.props);
+    this.state = {
+      currentVideo: window.exampleVideoData[0],
+      videos: window.exampleVideoData
+    };
+  }
+  
+  render() {
+    // console.log(this.state.currentVideo);
+    return (<div>
+      <Nav />
+      <div className="col-md-7">
+        <VideoPlayer videos={window.exampleVideoData[0]}/>
+      </div>
+      <div className="col-md-5">
+        <VideoList/>
+      </div>
+    </div>);
+  }
+
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-window.App = App;
+window.App = App({videos: window.exampleVideoData});
