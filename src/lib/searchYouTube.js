5bf62b0f715b96173ad50c76f4c7c2a11d5a9238
solomontang <solomoncztang@gmail.com>
2017-06-02 17:33:31 -0700

    Implement searchYoutube.js to issue AJAX request to youtube API upon invokation

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,20 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      maxResults: options.max,
+      key: options.key,
+      q: options.query,
+      part: 'snippet',
+      videoEmbeddable: true,
+      type: 'video'
+    },
+    success: (data) => {
+      callback(data.items);
+    },
+    dataType: 'json'
+  });
 };
 
 window.searchYouTube = searchYouTube;
